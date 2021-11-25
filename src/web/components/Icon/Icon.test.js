4322880a625493ea6d2c2331cea202bd1cccde7f
Alejandro-Gonzalez <alejandro@fizzmod.com>
2021-11-25 13:02:42 -0300

    add test for icon and changes storybooks

--- src/web/components/Icon/Icon.test.js
+++ src/web/components/Icon/Icon.test.js
@@ -2,14 +2,26 @@ import React from 'react';
 import Icon from 'web/components/Icon';
 import { create } from 'react-test-renderer';
 
+const jsonMock = {
+	box: { path: 'M 10 10 H 90 V 90 H 10 L 10 10', size: 24 },
+	fakeIcon: {}
+};
+
+jest.mock('./icons.json', () => jsonMock);
+
 describe('Icon component', () => {
 	test('Should render correctly', () => {
-		const wrapper = create(<Icon name="eye" />);
-		expect(wrapper.exists()).toBe(true);
+		const wrapper = create(<Icon name="box" />);
+		expect(wrapper.toJSON()).toBeTruthy();
 	});
 
 	test('Should be null when an incorrect name is passed', () => {
 		const wrapper = create(<Icon name="saraza" />);
-		expect(wrapper.exists()).toBe(false);
+		expect(wrapper.toJSON()).toBeNull();
+	});
+
+	test('Should be null when the icon has no path.', () => {
+		const wrapper = create(<Icon name="fakeIcon" />);
+		expect(wrapper.toJSON()).toBeNull();
 	});
 });
