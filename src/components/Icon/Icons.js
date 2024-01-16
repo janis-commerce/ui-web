6abe1fcba80c12b7dec5994642a4940d3223c502
mauroskyer <mauro.arteaga@janis.im>
2024-01-16 16:17:06 -0500

    Add search functionality on Icons

--- src/components/Icon/Icons.js
+++ src/components/Icon/Icons.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { docz as styled } from './styles';
 import icons from './icons.json';
@@ -7,17 +7,28 @@ import Icon from './Icon';
 import { validColors } from 'components/Button/utils';
 
 const Icons = ({ color }) => {
+	const [iconName, setIconName] = useState('');
 	const names = Object.keys(icons);
 
+	const filteredNames = names.filter((name) => name.toLowerCase().includes(iconName.toLowerCase()));
+
 	return (
-		<styled.Grid style={{ width: '100%' }}>
-			{names.map((name) => (
-				<styled.Item key={name} onClick={() => copyToClipboard(name)}>
-					<div>{name}</div>
-					<Icon name={name} color={color} />
-				</styled.Item>
-			))}
-		</styled.Grid>
+		<div>
+			<styled.SearchInput
+				type="text"
+				placeholder="Buscar por nombre..."
+				value={iconName}
+				onChange={(e) => setIconName(e.target.value)}
+			/>
+			<styled.Grid style={{ width: '100%' }}>
+				{filteredNames.map((name) => (
+					<styled.Item key={name} onClick={() => copyToClipboard(name)}>
+						<div>{name}</div>
+						<Icon name={name} color={color} />
+					</styled.Item>
+				))}
+			</styled.Grid>
+		</div>
 	);
 };
 
