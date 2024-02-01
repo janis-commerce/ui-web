89b1325194ca0dd8f1c8d61bd368dc00e7913e7f
Damian <damian.giannico@fizzmod.com>
2024-02-01 16:22:31 -0300

    change Icon and control and actions config

---
+++ .storybook/theme.js
@@ -0,0 +1,11 @@
+import {create} from '@storybook/theming';
+
+export default create({
+	base: 'light',
+	brandTitle: 'Janis UI Web',
+	brandUrl: 'https://janis.im',
+	brandImage: 'https://app.janis.in/static/media/janis-logo-base.0cc15e53.svg',
+	brandTarget: 'https://janis.im',
+	fontBase: '"Roboto", sans-serif',
+	fontCode: 'Roboto',
+});
