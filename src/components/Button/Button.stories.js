02c3de5de3ff5b8411474dc786d37b783bb92ee2
Damian <damian.giannico@fizzmod.com>
2024-02-19 09:10:46 -0300

    corrections on button styles

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -33,10 +33,8 @@ const Template = (args) => <Button {...args} />;
 const baseArgs = {
 	children: 'Button',
 	hideLabel: false,
-	color: 'blue',
 	iconColor: 'black',
 	iconSize: 24,
-	fontColor: 'white',
 	rounded: false,
 	variant: 'contained'
 };
