8d49b351b2ece80242689f9ef6c54a173e9e8440
Damian <damian.giannico@fizzmod.com>
2024-04-03 18:25:58 -0300

    add mixin

--- src/components/Avatar/styles.js
+++ src/components/Avatar/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import mixins from 'theme/mixins';
 import viewsPalette from 'theme/palette';
 
 export default {
@@ -6,13 +7,11 @@ export default {
 		width: ${({ size }) => size};
 		height: ${({ size }) => size};
 		border-radius: ${({ rounded }) => (rounded ? '50%' : '3px')};
-		display: flex;
-		justify-content: center;
-		align-items: center;
 		font-weight: 500;
 		color: ${viewsPalette.white};
 		background-color: ${({ color }) => color};
 		text-transform: uppercase;
+		${mixins.flexCenter};
 	`,
 	Image: styled.img`
 		width: ${({ size }) => size};
