6fb5ce91ce09fa1444c8b0b238896e541520fd1c
Fernando Colom <fernando_colom@janis.im>
2025-02-13 13:07:31 -0300

    add validation with lyfecycle event

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,9 +7,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.28.1] - 2025-02-13
+
 ### Fixed
 
 - Import map component
+- Script preinstall
 
 ## [0.28.0] - 2025-02-12
 
--- scripts/preinstall-script.js
+++ scripts/preinstall-script.js
@@ -1,3 +1,9 @@
+const isRootInstall = process.env.npm_lifecycle_event === 'preinstall';
+
+if (!isRootInstall) {
+	process.exit(0); // No ejecutar si se está instalando como dependencia
+}
+
 if (process.env.npm_execpath.indexOf('yarn') === -1) {
 	console.error('You must use Yarn to install dependencies:');
 	console.error('  $ yarn install');
