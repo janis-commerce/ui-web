3eabd159a46b6349f436a2414ac15f62a040a48f
Sebastian Vitiello <sebastian.vitiello@janis.im>
2025-02-21 11:09:32 -0300

    REMOVE: DIRECTIONS_URL, MAX_WAYPOINTS_PER_REQUEST constants

--- src/components/Map/utils/constants.js
+++ src/components/Map/utils/constants.js
@@ -5,9 +5,6 @@ export const INITIAL_CONTROLS_POSITION = {
 	fullScreen: 0
 };
 
-export const DIRECTIONS_URL = 'https://routes.googleapis.com/directions/v2:computeRoutes';
-export const MAX_WAYPOINTS_PER_REQUEST = 25;
-
 export const MAP_STYLERS = {
 	visibility: {
 		on: 'on',
