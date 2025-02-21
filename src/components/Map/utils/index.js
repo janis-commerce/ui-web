ccb7405f1c1ba48ca943f6041e5ee28df87c0466
Sebastian Vitiello <sebastian.vitiello@janis.im>
2025-02-21 11:05:55 -0300

    CHANGE: export functions method

--- src/components/Map/utils/index.js
+++ src/components/Map/utils/index.js
@@ -1,29 +1,13 @@
-import findSelectedPosition from './findSelectedPosition';
-import getBounds from './getBounds';
-import getBoundsFromMarkers from './getBoundsFromMarkers';
-import getCenterByGeolocationOrCenter from './getCenterByGeolocationOrCenter';
-import getGeolocationCoordinates from './getGeolocationCoordinates';
-import getMapOptions from './getMapOptions';
-import getMapStylers from './getMapStylers';
-import getParsedComponents from './getParsedComponents';
-import isValidLocation from './isValidLocation';
-import parseAddressComponents from './parseAddressComponents';
-import parseCoordsForPolylines from './parseCoordsForPolylines';
-import parsePlaces from './parsePlaces';
-import validateCoordinates from './validateCoordinates';
-
-export {
-	findSelectedPosition,
-	getBounds,
-	getBoundsFromMarkers,
-	getCenterByGeolocationOrCenter,
-	getGeolocationCoordinates,
-	getMapOptions,
-	getMapStylers,
-	getParsedComponents,
-	parseAddressComponents,
-	parseCoordsForPolylines,
-	parsePlaces,
-	validateCoordinates,
-	isValidLocation
-};
+export { default as findSelectedPosition } from './findSelectedPosition';
+export { default as getBounds } from './getBounds';
+export { default as getBoundsFromMarkers } from './getBoundsFromMarkers';
+export { default as getCenterByGeolocationOrCenter } from './getCenterByGeolocationOrCenter';
+export { default as getGeolocationCoordinates } from './getGeolocationCoordinates';
+export { default as getMapOptions } from './getMapOptions';
+export { default as getMapStylers } from './getMapStylers';
+export { default as getParsedComponents } from './getParsedComponents';
+export { default as isValidLocation } from './isValidLocation';
+export { default as parseAddressComponents } from './parseAddressComponents';
+export { default as parseCoordsForPolylines } from './parseCoordsForPolylines';
+export { default as parsePlaces } from './parsePlaces';
+export { default as validateCoordinates } from './validateCoordinates';
