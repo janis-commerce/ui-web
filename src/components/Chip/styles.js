6a2b07dab17302e5f615d8370121eaad6af1119a
Damian <damian.giannico@fizzmod.com>
2025-09-12 18:11:51 -0300

    fix: delete fixed maxwidth at chip to maxWidthProp

--- src/components/Chip/styles.js
+++ src/components/Chip/styles.js
@@ -78,7 +78,7 @@ export default {
 		height: ${(props) => (!props.onlyIcon ? '32px' : '36px')};
 		width: ${(props) => props.onlyIcon && '36px'};
 		min-width: 36px;
-		max-width: 150px;
+		${(props) => props.maxWidth && `max-width: ${props.maxWidth}`};
 		border-radius: ${(props) => (!props.onlyIcon ? '50px' : '50%')};
 		display: inline-flex;
 		justify-content: center;
