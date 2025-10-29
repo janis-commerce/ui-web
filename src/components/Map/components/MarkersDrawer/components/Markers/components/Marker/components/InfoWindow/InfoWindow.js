c120cdf116f37c2a8d583859ba3401096c28e795
Sebastian Vitiello <sebastian.vitiello@janis.im>
2025-10-29 14:58:05 -0300

    ADD: useMemo hook to pixelOffset variable - CHANGE: position property name replaced by infoWindowPosition

--- src/components/Map/components/MarkersDrawer/components/Markers/components/Marker/components/InfoWindow/InfoWindow.js
+++ src/components/Map/components/MarkersDrawer/components/Markers/components/Marker/components/InfoWindow/InfoWindow.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { InfoWindow as InfoWindowComponent } from '@react-google-maps/api';
 import { isNumber } from 'utils';
 import styled from './styles';
 
 const InfoWindow = ({ data, infoWindowOptions = {}, infoWindowHandles, children }) => {
-	const { position = {} } = infoWindowOptions;
-	const pixelOffset = new window.google.maps.Size(
-		isNumber(position?.x) ? position?.x : 0,
-		isNumber(position?.y) ? position?.y : -35
+	const { infoWindowPosition = {} } = infoWindowOptions;
+	const pixelOffset = useMemo(
+		() =>
+			new window.google.maps.Size(
+				isNumber(infoWindowPosition?.x) ? infoWindowPosition?.x : 0,
+				isNumber(infoWindowPosition?.y) ? infoWindowPosition?.y : -35
+			),
+		[infoWindowPosition?.x, infoWindowPosition?.y]
 	);
 
 	return (
