ad8826cbc207208d3f25f209bbdbd5d79b889ef9
Sebastian Vitiello <sebastian.vitiello@janis.im>
2025-11-04 12:13:38 -0300

    ADD: react-collapsed package, .mjs file support in storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,19 @@ module.exports = {
 	],
 	webpackFinal: async (config) => {
 		config.resolve.modules = [path.resolve(process.cwd(), 'src'), 'node_modules'];
+
+		// .mjs files support
+		config.module.rules.push({
+			test: /\.mjs$/,
+			include: /node_modules/,
+			type: 'javascript/auto'
+		});
+		const babelRule = config.module.rules.find(
+			(rule) => rule.test && rule.test.toString().includes('jsx')
+		);
+		if (babelRule) babelRule.exclude = /node_modules\/(?!react-collapsed)/;
+		config.resolve.extensions.push('.mjs');
+
 		return config;
 	}
 };
